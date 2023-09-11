7885f970e17649afe4eddc5641658433e833a875
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-11 23:03:33 +0700

    chore: datatypes in dart

---
+++ bin/data_types.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  String firstName = "Anin";
+  int shoesSize = 39;
+  double eWalletBalance = 2000.000; 
+  num haters = -1;
+  bool isMommyable = true;
+  List myOshi = ['Azizi', 'Ashel', 'Indira'];
+  Map couple = {
+    "wife": "Gracia",
+    "husband": "Yudist"
+  };
+}
