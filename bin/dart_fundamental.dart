4d6a963ad077e49903962dcb67e34f335aca4276
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-11 22:31:46 +0700

    chore: write a comment on dart

--- bin/dart_fundamental.dart
+++ bin/dart_fundamental.dart
@@ -1,5 +1,18 @@
 import 'package:dart_fundamental/dart_fundamental.dart' as dart_fundamental;
 
+/// Fungsi [main] akan menampilkan 2 output
+/// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada fungsi [calculate]
 void main(List<String> arguments) {
-  print('Hello world: ${dart_fundamental.calculate()}!');
+  // Mencetak Hello Dart! Dart is great. pada konsol
+  print('Hello Dart! Dart is great.');
+  // Testing documentation comment with [].
+  print('6 * 7 = ${dart_fundamental.calculate()}');
 }
+
+int calculate() {
+  return 6 * 7;
+}
+
+/// Output:
+/// Hello Dart! Dart is great.
+/// 6 * 7 = 42
