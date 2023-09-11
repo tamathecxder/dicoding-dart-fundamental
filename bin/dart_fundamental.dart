98766151fd3d994ebcfbd852190a7ad766b0fab8
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-11 22:21:35 +0700

    initial commit

---
+++ bin/dart_fundamental.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fundamental/dart_fundamental.dart' as dart_fundamental;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_fundamental.calculate()}!');
+}
---
+++ test/dart_fundamental_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_fundamental/dart_fundamental.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
