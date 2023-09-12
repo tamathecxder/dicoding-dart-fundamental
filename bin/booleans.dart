0b4a408950e6e4ce4d26930ff34acf214a2e9032
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-12 08:04:21 +0700

    chore: booleans in dart

---
+++ bin/booleans.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
+  var notTrue = !true;
+  bool notFalse = !false;
+
+  bool isWibu = false;
+  if (isWibu) {
+    print("It's true");
+  } else {
+    print("It's False");
+  }
+}
