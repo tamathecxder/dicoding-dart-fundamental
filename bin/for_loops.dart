2908342e68af77a0075564c2a87e5f68d319ced6
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-12 22:52:15 +0700

    chore: for loops in dart

---
+++ bin/for_loops.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  for (int i = 1; i <= 10; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write('* ');
+    }
+
+    print('');
+  }
+}
