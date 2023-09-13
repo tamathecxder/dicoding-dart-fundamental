f58aeec35b727bd7456e45775cf4763704a1b3e3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-13 22:20:53 +0700

    chore: property and method in dart class

---
+++ bin/property_and_method.dart
@@ -0,0 +1,10 @@
+import 'class.dart';
+
+void main(List<String> args) {
+  var dicodingCat = Animal('Gray', 2, 4.2);
+
+  dicodingCat.eat();
+  dicodingCat.poop();
+
+  print(dicodingCat.weight);  
+}
