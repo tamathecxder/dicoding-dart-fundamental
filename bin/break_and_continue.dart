2b16e6791248136d1db76f900b2233df35138315
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-13 20:55:41 +0700

    chore: break and continue keyword in dart

---
+++ bin/break_and_continue.dart
@@ -0,0 +1,33 @@
+void main() {
+  // bilangan prima di bawah 100
+  var primeNumbers = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 73, 79, 83, 89, 97
+  ];
+
+  var searchNumber = 13;
+  print('Masukkan bilangan prima di antara 1-100: $searchNumber');
+
+  for (int i = 0; i < primeNumbers.length; i++) {
+    if (searchNumber == primeNumbers[i]) {
+      print('$searchNumber adalah bilangan prima ke-${i + 1}');
+      break;
+    }
+    print('$searchNumber != ${primeNumbers[i]}');
+  }
+
+  for (int i = 1; i <= 10; i++) {
+    if (i % 3 == 0) {
+      continue;
+    }
+    print(i);
+  }
+}
+
+/// Output:
+/// Masukkan bilangan prima : 13
+/// 13 != 2
+/// 13 != 3
+/// 13 != 5
+/// 13 != 7
+/// 13 != 11
+/// 13 adalah bilangan prima ke-6
